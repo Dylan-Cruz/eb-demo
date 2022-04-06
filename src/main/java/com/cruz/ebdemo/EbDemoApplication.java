46d30c97b960b60421233516eb74f62eac89dab8
Dylan Cruz <dylan-cruz@live.com>
2022-04-06 15:15:02 -0400

    move the builder in, update build.gradle

--- src/main/java/com/cruz/ebdemo/EbDemoApplication.java
+++ src/main/java/com/cruz/ebdemo/EbDemoApplication.java
@@ -3,11 +3,19 @@ package com.cruz.ebdemo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @Slf4j
-public class EbDemoApplication {
+public class EbDemoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(EbDemoApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EbDemoApplication.class);
+	}
 }
+
